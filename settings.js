38d3c134c21065b382152423fa987fd5e28d4cc8
prabhujayalakshmi <65580141+prabhujayalakshmi@users.noreply.github.com>
2022-06-24 15:17:36 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x5779B2eDC601A34C467229FB0123c40Cb149e172";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
